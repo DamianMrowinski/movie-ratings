a527510183f2dac3a4033b0e14912f3221bb6ed5
Damian Mrowinski <damian.mrowinski@yahoo.com>
2019-12-15 20:06:04 +0100

    Initial backend project structure

---
+++ book-ratings-backend/src/main/java/pl/damianmrowinski/bookratingsbackend/BookRatingsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.damianmrowinski.bookratingsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRatingsBackendApplication.class, args);
+    }
+
+}
