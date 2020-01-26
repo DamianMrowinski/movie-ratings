4530d82ae15db7a880a269676b811310979e9616
Damian Mrowinski <damian.mrowinski@yahoo.com>
2020-01-27 00:07:44 +0100

    Add base entities

--- book-ratings-backend/src/main/java/pl/damianmrowinski/bookratingsbackend/domain/MovieEntity.java
+++ book-ratings-backend/src/main/java/pl/damianmrowinski/bookratingsbackend/domain/MovieEntity.java
@@ -1,23 +1,40 @@
 package pl.damianmrowinski.bookratingsbackend.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "movie")
 public class MovieEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
+    private String description;
+
+    private Integer year;
+
+    private String director;
+
+    private String country;
+
+    @OneToMany(mappedBy = "movie")
+    private List<RatingEntity> ratings = new ArrayList<>();
+
     protected MovieEntity() {
     }
 
-    public MovieEntity(String title) {
+    public MovieEntity(String title, String description, Integer year, String director, String country, List<RatingEntity> ratings) {
         this.title = title;
+        this.description = description;
+        this.year = year;
+        this.director = director;
+        this.country = country;
+        this.ratings = ratings;
     }
 
     public Long getId() {
@@ -26,5 +43,25 @@ public class MovieEntity {
 
     public String getTitle() {
         return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public List<RatingEntity> getRatings() {
+        return ratings;
     }
 }
